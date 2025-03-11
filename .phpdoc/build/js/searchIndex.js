52d6f20fe2a07a627eaf1d169c464d24dce170ef
Ayoub <ayoubsaada10@gmail.com>
2025-03-11 09:32:55 +0100

    Inicialización del proyecto primera modificación

---
+++ .phpdoc/build/js/searchIndex.js
@@ -0,0 +1,24 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\saludar\u0028\u0029",
+            "name": "saludar",
+            "summary": "Devuelve\u0020un\u0020saludo",
+            "url": "namespaces/default.html#function_saludar"
+        },                {
+            "fqsen": "\\sumar\u0028\u0029",
+            "name": "sumar",
+            "summary": "Suma\u0020dos\u0020n\u00FAmeros",
+            "url": "namespaces/default.html#function_sumar"
+        },                {
+            "fqsen": "\\fechaActual\u0028\u0029",
+            "name": "fechaActual",
+            "summary": "Devuelve\u0020la\u0020fecha\u0020actual",
+            "url": "namespaces/default.html#function_fechaActual"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Funciones PHP</title>
+</head>
+<body>
+    <button onclick="alert('<?php echo saludar(); ?>')">Saludar</button>
+    <button onclick="alert('<?php echo sumar(3, 4); ?>')">Sumar 3 y 4</button>
+    <button onclick="alert('<?php echo fechaActual(); ?>')">Mostrar Fecha Actual</button>
+
+    <?php
+    /**
+     * Devuelve un saludo
+     * 
+     * @return string
+     */
+    function saludar() {
+        return "¡Hola, mundo!";
+    }
+
+    /**
+     * Suma dos números
+     * 
+     * @param int $a El primer n mero
+     * @param int $b El segundo n mero
+     * 
+     * @return int La suma de los dos n meros
+     */
+    function sumar($a, $b) {
+        return $a + $b;
+    }
+
+    /**
+     * Devuelve la fecha actual
+     * 
+     * @return string La fecha actual en formato AAAA-MM-DD
+     */
+    function fechaActual() {
+        return date('Y-m-d');
+    }
+    ?>
+</body>
+</html>
+
